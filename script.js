8bbae7b8f26145ecef9b14a3e3e3825a0ab81981
dsabireeee <138918318+dsabireeee@users.noreply.github.com>
2024-06-09 20:33:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function gg() {
+  document.getElementById("pp").innerText = "Thank you for being the best man ever & for being patient with me & what my family puts u trought u are my fav person ilysm ";
+}
